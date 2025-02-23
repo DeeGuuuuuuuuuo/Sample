d218639af3e981f204c972d3e4f677e99dfe78a2
DeeGuuuuuuuuuo <dee.guo@bamboonetworks.com>
2025-02-23 23:08:12 +0800

    Update Sample.js

--- Sample.js
+++ Sample.js
@@ -1,53 +1,48 @@
-function getMultipleEnvironmentVariableValues(variableSchemaNames, callback) {
-    // 查询多个环境变量的定义
-    var filter = variableSchemaNames.map(name => "schemaname eq '" + name + "'").join(" or ");
-    var query = "/api/data/v9.2/environmentvariabledefinitions?$filter=" + filter + "&$select=environmentvariabledefinitionid,schemaname,defaultvalue";
+function getEnvironmentVariableValue(variableSchemaName, callback) {
+    // 查询环境变量的定义
+    var query = "/api/data/v9.2/environmentvariabledefinitions?$filter=schemaname eq '" + variableSchemaName + "'&$select=environmentvariabledefinitionid,defaultvalue";
 
     Xrm.WebApi.retrieveMultipleRecords("environmentvariabledefinition", query).then(
         function success(result) {
             if (result.entities.length > 0) {
-                var variableDefinitions = result.entities;
-                var variableValues = {};
+                var variableId = result.entities[0].environmentvariabledefinitionid;
+                var defaultValue = result.entities[0].defaultvalue;
 
-                // 查询每个环境变量的值
-                var promises = variableDefinitions.map(function(variable) {
-                    var valueQuery = "/api/data/v9.2/environmentvariablevalues?$filter=_environmentvariabledefinitionid_value eq " + variable.environmentvariabledefinitionid + "&$select=value";
+                // 查询环境变量的值
+                var valueQuery = "/api/data/v9.2/environmentvariablevalues?$filter=_environmentvariabledefinitionid_value eq " + variableId + "&$select=value";
 
-                    return Xrm.WebApi.retrieveMultipleRecords("environmentvariablevalue", valueQuery).then(
-                        function success(valueResult) {
-                            if (valueResult.entities.length > 0) {
-                                // 存储环境变量的值
-                                variableValues[variable.schemaname] = valueResult.entities[0].value;
-                            } else {
-                                // 如果没有设置值，使用默认值
-                                variableValues[variable.schemaname] = variable.defaultvalue;
-                            }
-                        },
-                        function error(error) {
-                            console.error("查询环境变量值时出错:", error);
-                            variableValues[variable.schemaname] = null;
+                Xrm.WebApi.retrieveMultipleRecords("environmentvariablevalue", valueQuery).then(
+                    function success(valueResult) {
+                        if (valueResult.entities.length > 0) {
+                            // 返回环境变量的值
+                            callback(valueResult.entities[0].value);
+                        } else {
+                            // 如果没有设置值，返回默认值
+                            callback(defaultValue);
                         }
-                    );
-                });
-
-                // 等待所有查询完成
-                Promise.all(promises).then(function() {
-                    callback(variableValues);
-                });
+                    },
+                    function error(error) {
+                        console.error("查询环境变量值时出错:", error);
+                        callback(null);
+                    }
+                );
             } else {
-                console.error("未找到环境变量定义。");
-                callback({});
+                console.error("未找到环境变量定义:", variableSchemaName);
+                callback(null);
             }
         },
         function error(error) {
             console.error("查询环境变量定义时出错:", error);
-            callback({});
+            callback(null);
         }
     );
 }
 
 // 使用示例
-var variableSchemaNames = ["Variable1", "Variable2", "Variable3"];
-getMultipleEnvironmentVariableValues(variableSchemaNames, function(values) {
-    console.log("环境变量的值:", values);
+getEnvironmentVariableValue("YourVariableSchemaName", function(value) {
+    if (value !== null) {
+        console.log("环境变量的值是:", value);
+    } else {
+        console.log("未找到环境变量或值。");
+    }
 });
